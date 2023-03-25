9e43960ed7cdaa4bbe991e1557b1f5483cb62029
Daksh <dakshkant8@gmail.com>
2023-03-25 11:45:38 +0530

    added API for receving PDFs

--- keyword_api/main.py
+++ keyword_api/main.py
@@ -1,6 +1,11 @@
 from collections import defaultdict
 from flask import Flask, request, jsonify
 from json import loads
+import base64
+import io
+import PyPDF2
+import pytesseract
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -10,11 +15,11 @@ def home():
 
 
 # collection = {"text": "TEXT_FROM_USER"}
-@app.route("/getkeywords", methods=["POST"])
+@app.route("/get-keywords", methods=["POST"])
 def getKeywords():
     try:
-        variable = request.get_data()
-        text = loads(variable)
+        variable = request.get_data()   # get data from body in terms of bytes
+        text = loads(variable)          # convert the bytes into a Python object
         # print(type(text))
         # print(text)
 
@@ -28,16 +33,52 @@ def getKeywords():
     except:
         return jsonify({"message": "Invalid format"})
 
+@app.route("/pdf-to-text", methods=["POST"])
+def getPdf():
+    try:
+        variable = request.get_data()
+        encodedFile = loads(variable)
+        pdfBytes = base64.b64decode(encodedFile)
+        pdfIO = io.BytesIO(pdfBytes)
+
+        pdfReader = PyPDF2.PdfFileReader(pdfIO)
+        pdfText = ""
+
+        for pageNum in range(pdfReader.getNumPages()):
+            page = pdfReader.getPage(pageNum)
+            pageText = page.extractText()
+
+            if not pageText:
+                # if the page is blank, we do OCR
+                pageImage = page.getPixmap().getImage()
+                pageImageBytes = io.BytesIO()
+                pageImage.writeImage(pageImageBytes)
+                pageImageBytes.seek(0)
+
+                pageImagePIL = Image.open(pageImageBytes)
+                pageTextOCR = pytesseract.image_to_string(pageImagePIL)
+                pdfText += (pageTextOCR + " ")
+
+            else:
+                pdfText += (pageText + " ")
+
+        dataToReturn = getKeywords(pdfText)
+        return jsonify({"data": dataToReturn})
+        
+    except:
+        return jsonify({"message": "Invalid format"})
+
 
 from rake_nltk import Rake
 
 def extractKeywords(text: str):
-    r = Rake()
+    r = Rake(max_length=100000000000000000000000000000000000)
     r.extract_keywords_from_text(text)
     data = defaultdict(list)
     for rating, keyword in r.get_ranked_phrases_with_scores():
         data[rating].append(keyword)
 
+    data = sorted(data.items(), key=lambda x: x[0], reverse=True)    
     return data
 
 
